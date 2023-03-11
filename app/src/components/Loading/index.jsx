d18d33c0971f0d15ce4d19d067568a8065274c92
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-11 20:42:16 -0300

    add Loading

---
+++ app/src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import { LoadingContainer, Title } from "./styled";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <LoadingContainer>
+      <Title>Loading...</Title>
+    </LoadingContainer>
+  );
+}
