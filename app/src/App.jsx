012ac8741a56b50a87fc56b31d7eef9de1cb6a34
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-11 20:42:38 -0300

    add react router dom

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
+import Details from "./pages/Details";
 import Home from "./pages/Home";
+
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Switch>
+      </Router>
     </>
   );
 }
