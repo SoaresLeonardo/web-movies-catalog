ee51f1f36c096812e082dbd1a0db8d14a4355964
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-11 16:20:37 -0300

    Footer

---
+++ app/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContainerFooter, Content, Title } from "./styled";
+
+export default function Footer() {
+  return (
+    <ContainerFooter>
+      <Content>
+        <Title>© Feito por Leonardo</Title>
+      </Content>
+    </ContainerFooter>
+  );
+}
---
+++ app/src/components/Footer/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerFooter = styled.div`
+  margin-top: 50px;
+  background-color: #151515;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Content = styled.div``;
+export const Title = styled.p``;
