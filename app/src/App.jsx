fa23dcd586a824e91f53c196755afa1957162297
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-11 16:20:55 -0300

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Footer/>
     </>
   );
 }
