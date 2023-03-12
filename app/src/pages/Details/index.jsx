db43d63c6a64c80a1f75aa0c2a15a3ed717a97c0
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-03-12 00:04:07 -0300

    Update index.jsx

--- app/src/pages/Details/index.jsx
+++ app/src/pages/Details/index.jsx
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from "react";
 
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import {
+  Container,
+  Content,
+  Description,
+  DetailsImage,
+  DetailsIntro,
+  Duration,
+  ExitButton,
+  ReleaseDate,
+  Title,
+} from "./styled";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const apiUrl = import.meta.env.VITE_DETAILS;
 
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import Loading from "../../components/Loading";
+
 export default function Details() {
   const { id } = useParams();
   const [dataMovie, setDataMovie] = useState({});
+  const movieImage = "https://image.tmdb.org/t/p/w500";
   console.log(dataMovie);
+
   const [loading, setLoading] = useState(false);
+
   const fetchData = async (url) => {
     setLoading(true);
     try {
@@ -18,7 +35,10 @@ export default function Details() {
       const movie = {
         id,
         title: data.title,
-        sinopse: data.sinopse,
+        overview: data.overview,
+        image: `${movieImage}${data.poster_path}`,
+        releaseDate: data.release_date,
+        runtime: data.runtime,
       };
       setLoading(false);
       setDataMovie(movie);
@@ -29,13 +49,34 @@ export default function Details() {
   };
 
   useEffect(() => {
+    if (!id) return;
     const movie = `${apiUrl}${id}?api_key=${apiKey}`;
     fetchData(movie);
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <>
-      <h1>{dataMovie.title}</h1>
-    </>
+    <Container>
+      <Content>
+        <DetailsImage>
+          <img src={dataMovie.image} alt={dataMovie.title} />
+        </DetailsImage>
+        <DetailsIntro>
+          <Title>{dataMovie.title}</Title>
+          <Description>{dataMovie.overview}</Description>
+          <Duration>{`${dataMovie.runtime} minutes`}</Duration>
+          <ReleaseDate>{dataMovie.releaseDate}</ReleaseDate>
+          <Link to="/">
+            <ExitButton>
+              <AiOutlineArrowLeft style={{ marginRight: 5 }} />
+              Go Back
+            </ExitButton>
+          </Link>
+        </DetailsIntro>
+      </Content>
+    </Container>
   );
 }
